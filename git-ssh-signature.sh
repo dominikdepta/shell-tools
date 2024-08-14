d5f57d74db4b5bd961c782bfb4e4da2b8eb788e8
Dominik Depta <dominikdepta@gmail.com>
2024-08-14 08:49:39 +0200

    add git-ssh-signature.sh

---
+++ git-ssh-signature.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+while true; do
+  read -p "Enter SSH public key path (Ctrl + C to cancel): " ssh_key_path
+
+  expanded_path="${ssh_key_path/#~/$HOME}"
+
+  echo "Checking file at: '$expanded_path'"
+
+  if [ -f "$expanded_path" ]; then
+    break
+  else
+    echo "File not found. Try again."
+  fi
+done
+
+if [ ! -d ".git" ]; then
+  echo "Not a Git repository."
+  exit 1
+fi
+
+git config gpg.format ssh
+git config user.signingkey "$expanded_path"
+git config commit.gpgSign true
+
+echo "Git signing configured with key: $expanded_path"
